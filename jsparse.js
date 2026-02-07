10762a35f70740d9e7950e91329698bb8e870aa7
Jake Lazaroff <jake@phazein.com>
2026-02-07 16:32:14 -0500

    Initial commit

---
+++ jsparse.js
@@ -0,0 +1,325 @@
+// STANDARD SCHEMA TYPES
+// See https://standardschema.dev
+
+/**
+ * @template [Input = unknown]
+ * @template [Output = Input]
+ * @typedef {{ readonly "~standard": Props<Input, Output> }} StandardSchemaV1 The Standard Schema interface.
+ * See https://standardschema.dev.
+ */
+
+/**
+ * @template [Input = unknown]
+ * @template [Output = Input]
+ * @typedef {object} Props The Standard Schema properties interface.
+ * @property {1} version The version number of the standard.
+ * @property {string} vendor The vendor name of the schema library.
+ * @property {(value: unknown) => Result<Output>} validate Validates unknown input values.
+ * @property {Types<Input, Output> | undefined} [types] Inferred types associated with the schema.
+ */
+
+/**
+ * @template Output
+ * @typedef {SuccessResult<Output> | FailureResult} Result The result interface of the validate function.
+ */
+
+/**
+ * @template Output
+ * @typedef {object} SuccessResult The result interface if validation succeeds.
+ * @property {Output} value The typed output value.
+ * @property {undefined} [issues] The non-existent issues.
+ */
+
+/**
+ * @typedef {object} FailureResult The result interface if validation fails.
+ * @property {ReadonlyArray<Issue>} issues The issues of failed validation.
+ */
+
+/**
+ * @typedef {object} Issue The issue interface of the failure output.
+ * @property {string} message The error message of the issue.
+ * @property {ReadonlyArray<PropertyKey>} [path] The path of the issue, if any.
+ */
+
+/**
+ * @template [Input = unknown]
+ * @template [Output = Input]
+ * @typedef {object} Types The Standard Schema types interface.
+ * @property {Input} input The input type of the schema.
+ * @property {Output} output The output type of the schema.
+ */
+
+/**
+ * @template {StandardSchemaV1} Schema
+ * @typedef {NonNullable<Schema['~standard']['types']>['output']} InferOutput
+ */
+
+// IMPLEMENTATION
+
+const NS = "~standard";
+
+class SchemaError extends Error {
+  /** @param {readonly Issue[]} issues */
+  constructor(issues) {
+    const msg = issues
+      .map(({ message, path }) => (path.length ? `- $.${path.join(".")}: ${message}` : message))
+      .join("\n");
+    super(msg);
+  }
+}
+
+/**
+ * Creates a schema from a validation function.
+ * @template [Input = unknown]
+ * @template [Output = Input]
+ * @param {(input: any) => Result<Output>} validate The validation function.
+ * @returns {StandardSchemaV1<Input, Output>}
+ */
+function schema(validate) {
+  return {
+    [NS]: {
+      version: 1,
+      vendor: "",
+      validate,
+    },
+  };
+}
+
+/**
+ * Validates a given input against a schema.
+ * @template T
+ * @param {StandardSchemaV1<any, T>} schm
+ * @param {any} input
+ */
+const v = (schm, input) => schm[NS].validate(input);
+
+/**
+ * @template [Input = unknown]
+ * @template [Output = Input]
+ * @param {(value: unknown) => value is Output} fn The validation function.
+ * @param {(value: unknown) => string} msg
+ * @returns {StandardSchemaV1<Input, Output>}
+ */
+export function custom(fn, msg) {
+  return schema((value) => {
+    if (fn(value)) return { value };
+    return { issues: [{ message: msg(value), path: [] }] };
+  });
+}
+
+/**
+ * @param {string} expected
+ * @param {string} actual
+ */
+function formatError(expected, actual) {
+  return `Expected ${expected}, received \`${JSON.stringify(actual)}\``;
+}
+
+/** @param {string} expected */
+function formatErrorFor(expected) {
+  /** @param {any} actual */
+  return (actual) => formatError(expected, actual);
+}
+
+const _undefined = custom((x) => x === undefined, formatErrorFor("undefined"));
+const _null = custom((x) => x === null, formatErrorFor("null"));
+export { _undefined as undefined, _null as null };
+
+export const bigint = custom((x) => typeof x === "bigint", formatErrorFor("bigint"));
+export const boolean = custom((x) => typeof x === "boolean", formatErrorFor("boolean"));
+export const number = custom((x) => typeof x === "number", formatErrorFor("number"));
+export const string = custom((x) => typeof x === "string", formatErrorFor("string"));
+export const symbol = custom((x) => typeof x === "symbol", formatErrorFor("symbol"));
+export const any = custom(
+  /** @returns {_ is any} */ (_) => true,
+  () => "",
+);
+
+/**
+ * @template {StandardSchemaV1[]} T
+ * @param {T} schemata
+ * @returns {StandardSchemaV1<unknown, InferOutput<T[number]>>}
+ */
+export const union = (...schemata) => {
+  return schema((input) => {
+    /** @type {Issue[]} */
+    let issues = [];
+
+    for (const schm of schemata) {
+      const result = v(schm, input);
+      if (!result.issues) return result;
+      issues = issues.concat(result.issues);
+    }
+
+    return { issues };
+  });
+};
+
+/**
+ * Converts a union type to an intersection type;
+ * see https://fettblog.eu/typescript-union-to-intersection/
+ * @template T
+ * @typedef {(T extends any ? (x: T) => any : never) extends (x: infer R) => any ? R : never} UnionToIntersection
+ */
+
+/**
+ * @template {StandardSchemaV1[]} T
+ * @param {T} schemata
+ * @returns {StandardSchemaV1<unknown, UnionToIntersection<InferOutput<T[number]>>>}
+ */
+export const intersect = (...schemata) => {
+  return schema((value) => {
+    /** @type {Issue[]} */
+    let issues = [];
+
+    for (const schm of schemata) {
+      const result = v(schm, value);
+      if (result.issues) issues = issues.concat(result.issues);
+    }
+
+    return issues.length ? { issues } : { value };
+  });
+};
+
+/**
+ * @template {StandardSchemaV1} T
+ * @param {T} schm
+ */
+export const optional = (schm) => union(_undefined, schm);
+
+/**
+ * @template {StandardSchemaV1} T
+ * @param {T} schm
+ */
+export const nullable = (schm) => union(_null, schm);
+
+/**
+ * @template T
+ * @param {T} lit
+ */
+export const literal = (lit) =>
+  custom(
+    /** @returns {v is T} */
+    (v) => v === lit,
+    formatErrorFor(`\`${lit}\``),
+  );
+
+/**
+ * @template {Function} T
+ * @param {T} fn
+ */
+export const instance = (fn) =>
+  custom(
+    /** @returns {v is InstanceType<T>} */
+    (v) => v instanceof fn,
+    formatErrorFor(`instance of ${fn.name}`),
+  );
+
+/**
+ * @template T
+ * @param {StandardSchemaV1<unknown, T>} schm
+ * @returns {StandardSchemaV1<unknown, T[]>}
+ */
+export const array = (schm) => {
+  return schema((input) => {
+    if (!Array.isArray(input)) return { issues: [{ message: formatError("array", input) }] };
+
+    /** @type {Issue[]} */
+    const issues = [];
+
+    for (const [i, val] of input.entries()) {
+      const result = v(schm, val);
+      if (result.issues) {
+        for (const issue of result.issues) {
+          issue.path = /** @type {PropertyKey[]} */ ([i]).concat(issue.path || []);
+          issues.push(issue);
+        }
+      }
+    }
+
+    if (issues.length) return { issues };
+    return /** @type {SuccessResult<typeof values>} */ ({ value: input });
+  });
+};
+
+/** @param {any} input */
+function isObject(input) {
+  return typeof input === "object" && input !== null && !Array.isArray(input);
+}
+
+/**
+ * @template {{ [key: PropertyKey]: StandardSchemaV1}} T
+ * @param {T} schm
+ * @returns {StandardSchemaV1<unknown, { [K in keyof T]: InferOutput<T[K]> }>}
+ */
+export const object = (schm) => {
+  return schema((value) => {
+    if (!isObject(value)) return { issues: [{ message: formatError("object", value) }] };
+
+    /** @type {Issue[]} */
+    const issues = [];
+
+    for (const [key, subschm] of Object.entries(schm)) {
+      const result = v(subschm, value[key]);
+      if (result.issues) {
+        for (const issue of result.issues || []) {
+          issue.path = /** @type {[PropertyKey]} */ ([key]).concat(issue.path || []);
+          issues.push(issue);
+        }
+      }
+    }
+
+    return issues.length ? { issues } : { value };
+  });
+};
+
+/**
+ * @template T
+ * @param {StandardSchemaV1<unknown, T>} schm
+ * @returns {StandardSchemaV1<unknown, { [key: PropertyKey]: T }>}
+ */
+export const record = (schm) => {
+  return schema((value) => {
+    if (!isObject(value)) return { issues: [{ message: formatError("object", value) }] };
+
+    /** @type {Issue[]} */
+    const issues = [];
+
+    for (const [key, val] of Object.entries(value)) {
+      const result = v(schm, val);
+      if (result.issues) {
+        for (const issue of result.issues || []) {
+          issue.path = /** @type {[PropertyKey]} */ ([key]).concat(issue.path || []);
+          issues.push(issue);
+        }
+      }
+    }
+
+    return issues.length ? { issues } : { value };
+  });
+};
+
+/**
+ * @template [Input = unknown]
+ * @template [Output = Input]
+ * @param {StandardSchemaV1<Input, Output>} schm
+ * @param {unknown} input
+ * @throws {SchemaError}
+ * @returns {Output}
+ */
+export function parse(schm, input) {
+  const result = v(schm, input);
+  if (result.issues) throw new SchemaError(result.issues);
+  return result.value;
+}
+
+/**
+ * @template [Input = unknown]
+ * @template [Output = Input]
+ * @param {StandardSchemaV1<Input, Output>} schm
+ * @param {unknown} input
+ * @returns {input is Output}
+ */
+export function is(schm, input) {
+  return !v(schm, input).issues;
+}
---
+++ test.js
@@ -0,0 +1,213 @@
+import assert from "node:assert/strict";
+import { describe, test } from "node:test";
+
+import * as v from "./jsparse.js";
+
+/** @type {Record<string, [unknown, import("./validate.js").StandardSchemaV1]>} */
+const PRIMITIVES = {
+  bigint: [1n, v.bigint],
+  boolean: [true, v.boolean],
+  number: [1, v.number],
+  string: ["test", v.string],
+  symbol: [Symbol(), v.symbol],
+  null: [null, v.null],
+  undefined: [undefined, v.undefined],
+};
+
+describe("primitives", () => {
+  for (const [type, [value, schema]] of Object.entries(PRIMITIVES)) {
+    describe(type, () => {
+      test(`parses ${type}s`, () => {
+        assert.doesNotThrow(() => v.parse(schema, value));
+      });
+
+      test("doesn't parse other types", () => {
+        const values = Object.entries(PRIMITIVES)
+          .filter(([key]) => key !== type)
+          .map(([, [value]]) => value);
+        for (const value of values) {
+          assert.throws(() => v.parse(schema, value));
+        }
+      });
+    });
+  }
+});
+
+describe("literal", () => {
+  test("parses if value is the same as the literal", () => {
+    const schema = v.literal("test");
+    assert.doesNotThrow(() => v.parse(schema, "test"));
+  });
+
+  test("doesn't parse if value is not the same as the literal", () => {
+    const schema = v.literal("test");
+    assert.throws(() => v.parse(schema, "other"));
+  });
+});
+
+describe("instance", () => {
+  test("parses if value is an instance of the given class", () => {
+    class Test {}
+    const schema = v.instance(Test);
+    assert.doesNotThrow(() => v.parse(schema, new Test()));
+  });
+
+  test("doesn't parse if value is not an instance of the given class", () => {
+    class Test {}
+    const schema = v.instance(Test);
+    assert.throws(() => v.parse(schema, {}));
+  });
+});
+
+describe("array", () => {
+  test("parses if all elements match the given schema", () => {
+    const schema = v.array(v.string);
+    assert.doesNotThrow(() => v.parse(schema, ["one", "two"]));
+  });
+
+  test("doesn't parse if any element doesn't match the given schema", () => {
+    const schema = v.array(v.string);
+    assert.throws(() => v.parse(schema, ["one", 2]));
+  });
+
+  test("doesn't parse if not passed an array", () => {
+    const schema = v.array(v.string);
+    assert.throws(() => v.parse(schema, { 0: "one", 1: "two" }));
+  });
+});
+
+describe("record", () => {
+  test("parses if all properties match the given schema", () => {
+    const schema = v.record(v.string);
+    assert.doesNotThrow(() => v.parse(schema, { a: "one", b: "two" }));
+  });
+
+  test("doesn't parse if any property doesn't match the given schema", () => {
+    const schema = v.record(v.string);
+    assert.throws(() => v.parse(schema, { a: "one", b: 2 }));
+  });
+
+  test("doesn't parse if not passed an object", () => {
+    const schema = v.record(v.string);
+    assert.throws(() => v.parse(schema, ["one", "two"]));
+  });
+});
+
+describe("object", () => {
+  test("parses if each property matches the corresponding schema", () => {
+    const schema = v.object({ name: v.string, age: v.number });
+    assert.doesNotThrow(() => v.parse(schema, { name: "jake", age: 34 }));
+  });
+
+  test("doesn't parse if any property doesn't match the corresponding schema", () => {
+    const schema = v.object({ name: v.string, age: v.number });
+    assert.throws(() => v.parse(schema, { name: 34, age: 34 }));
+    assert.throws(() => v.parse(schema, { name: "jake", age: "jake" }));
+    assert.throws(() => v.parse(schema, { name: "jake" }));
+  });
+
+  test("doesn't parse if not passed an object", () => {
+    const schema = v.record(v.string);
+    assert.throws(() => v.parse(schema, ["one", "two"]));
+  });
+});
+
+describe("custom", () => {
+  test("parses if the validation function returns true", () => {
+    const schema = v.custom(
+      /** @returns {v is `${string}@${string}`} */
+      (v) => typeof v === "string" && v.includes("@"),
+      () => "error",
+    );
+    assert.doesNotThrow(() => v.parse(schema, "email@example.com"));
+  });
+
+  test("doesn't parse if the validation function returns false", () => {
+    const schema = v.custom(
+      /** @returns {v is `${string}@${string}`} */
+      (v) => typeof v === "string" && v.includes("@"),
+      () => "error",
+    );
+    assert.throws(() => v.parse(schema, "notanemail"));
+  });
+});
+
+describe("union", () => {
+  test("parses if value matches any of the schemas", () => {
+    const schema = v.union(v.string, v.number);
+    assert.doesNotThrow(() => v.parse(schema, "test"));
+    assert.doesNotThrow(() => v.parse(schema, 123));
+  });
+
+  test("doesn't parse if value doesn't match any schema", () => {
+    const schema = v.union(v.string, v.number);
+    assert.throws(() => v.parse(schema, true));
+  });
+
+  test("works with more than two schemas", () => {
+    const schema = v.union(v.string, v.number, v.boolean);
+    assert.doesNotThrow(() => v.parse(schema, "test"));
+    assert.doesNotThrow(() => v.parse(schema, 123));
+    assert.doesNotThrow(() => v.parse(schema, true));
+    assert.throws(() => v.parse(schema, null));
+  });
+
+  test("works with literal types", () => {
+    const schema = v.union(v.literal("red"), v.literal("green"), v.literal("blue"));
+    assert.doesNotThrow(() => v.parse(schema, "red"));
+    assert.doesNotThrow(() => v.parse(schema, "green"));
+    assert.doesNotThrow(() => v.parse(schema, "blue"));
+    assert.throws(() => v.parse(schema, "yellow"));
+  });
+
+  test("works with complex types", () => {
+    const schema = v.union(
+      v.object({ type: v.literal("user"), name: v.string }),
+      v.object({ type: v.literal("admin"), name: v.string, level: v.number }),
+    );
+    assert.doesNotThrow(() => v.parse(schema, { type: "user", name: "jake" }));
+    assert.doesNotThrow(() => v.parse(schema, { type: "admin", name: "jake", level: 5 }));
+  });
+});
+
+describe("intersect", () => {
+  test("merges object properties", () => {
+    const schema = v.intersect(v.object({ name: v.string }), v.object({ age: v.number }));
+    const result = v.parse(schema, { name: "jake", age: 34 });
+    assert.equal(result.name, "jake");
+    assert.equal(result.age, 34);
+  });
+
+  test("works with more than two schemas", () => {
+    const schema = v.intersect(
+      v.object({ name: v.string }),
+      v.object({ age: v.number }),
+      v.object({ email: v.string }),
+    );
+    const result = v.parse(schema, { name: "jake", age: 34, email: "j@e.com" });
+    assert.equal(result.name, "jake");
+    assert.equal(result.age, 34);
+    assert.equal(result.email, "j@e.com");
+  });
+
+  test("fails if any schema in intersection fails", () => {
+    const schema = v.intersect(v.object({ name: v.string }), v.object({ age: v.number }));
+    assert.throws(() => v.parse(schema, { name: "jake" }));
+    assert.throws(() => v.parse(schema, { age: 34 }));
+    assert.throws(() => v.parse(schema, { name: 123, age: "test" }));
+  });
+});
+
+describe("optional", () => {
+  test("works if the given type is undefined", () => {
+    const schema = v.optional(v.string);
+    assert.throws(() => v.parse(schema, 123));
+    assert.doesNotThrow(() => v.parse(schema, "jake"));
+    assert.doesNotThrow(() => v.parse(schema, undefined));
+  });
+
+  test("asfd", () => {
+    const schema = v.object({ test: v.optional(v.string) });
+    const foo = v.parse(schema, { test: "jake" });
+  });
+});
