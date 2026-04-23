7326c8a0e442ad88340f4353abb6afd3e22d05aa
Jake Lazaroff <jake@jakelazaroff.com>
2026-04-22 21:58:18 -0400

    Add MPL comment

--- validate.js
+++ validate.js
@@ -1,4 +1,10 @@
 // validate.js v0.1.1
+//
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at https://mozilla.org/MPL/2.0/.
+//
+// Copyright (c) 2026 Jake Lazaroff https://github.com/jakelazaroff/validate.js
 
 // STANDARD SCHEMA TYPES
 // See https://standardschema.dev
